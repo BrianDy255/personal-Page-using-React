a42d0c09b9bb46d0e955b52037c15f17ba8afa2f
Brian Dy <113547253+BrianDy255@users.noreply.github.com>
2023-03-31 13:37:32 -0700

    Add files via upload

--- pages/home.jsx
+++ pages/home.jsx
@@ -5,7 +5,7 @@ function Home() {
         <main>
             <section>
         <h1> Welcome to the Brian Dy homepage </h1>
-        <img src="../public/images/IMG_7497.jpg" className="profilepic"></img>
+        <img src="public/images/IMG_7497.jpg" className="profilepic"></img>
             </section>
             <section>
                 <p>A personal project creating a home page providing some details about my life as well as practice using the React framework. Feel free to take a look around the website!</p>
