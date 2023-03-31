fa0640ef54bc21c6055256e0b353cc8c5df588e1
Brian Dy <113547253+BrianDy255@users.noreply.github.com>
2023-03-31 14:14:41 -0700

    Add files via upload

--- components/cards.jsx
+++ components/cards.jsx
@@ -4,7 +4,7 @@ function Card(props) {
     return (
         <div >
             <p className="cardtitle">{props.item.title}</p>
-            <img src={`https://github.com/BrianDy255/personal-Page-using-React/blob/main/public/images/travel/${props.item.coverImg}`} className="travelpic"/>
+            <img src={`/images/travel/${props.item.coverImg}`} className="travelpic"/>
             <div className="carddescription">
                 <p>Location: {props.item.location}</p>
                 <p>Year Visited: {props.item.year}</p>
