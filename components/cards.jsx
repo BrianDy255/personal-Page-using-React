b803c68508f7a3d39346627d6bdbd8a0e463d773
Brian Dy <113547253+BrianDy255@users.noreply.github.com>
2023-03-31 13:50:46 -0700

    Add files via upload

--- components/cards.jsx
+++ components/cards.jsx
@@ -4,7 +4,7 @@ function Card(props) {
     return (
         <div >
             <p className="cardtitle">{props.item.title}</p>
-            <img src={`https://642744a59dfe1800086f28c3--candid-paprenjak-0ca4a2.netlify.app/public/images/travel/${props.item.coverImg}`} className="travelpic"/>
+            <img src={`https://github.com/BrianDy255/personal-Page-using-React/blob/main/public/images/travel/${props.item.coverImg}`} className="travelpic"/>
             <div className="carddescription">
                 <p>Location: {props.item.location}</p>
                 <p>Year Visited: {props.item.year}</p>
--- components/footer.jsx
+++ components/footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
             <p>Author: Brian Dy</p>
             <p><a href="mailto:solarisprime255@gmail.com">Email me</a></p>
             <div>
-                <a href="https://www.youtube.com/channel/UCpClSGdBFl9UyMXBqYlePAA"><img src="https://642744a59dfe1800086f28c3--candid-paprenjak-0ca4a2.netlify.app/public/icons/youtube.png" className="footericons" useMap="#footericons"></img></a>
+                <a href="https://www.youtube.com/channel/UCpClSGdBFl9UyMXBqYlePAA"><img src="https://github.com/BrianDy255/personal-Page-using-React/blob/main/public/icons/youtube.png" className="footericons" useMap="#footericons"></img></a>
             </div>
             
             
